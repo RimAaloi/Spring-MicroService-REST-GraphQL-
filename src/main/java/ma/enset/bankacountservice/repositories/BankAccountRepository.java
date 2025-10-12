9f180f5c09241505a6f1f2881bc6503a5cf93f2c
RimAaloi <aaloirim@example.com>
2025-10-12 21:23:19 +0200

    first commit

---
+++ src/main/java/ma/enset/bankacountservice/repositories/BankAccountRepository.java
@@ -0,0 +1,16 @@
+package ma.enset.bankacountservice.repositories;
+
+import ma.enset.bankacountservice.entities.BankAccount;
+import ma.enset.bankacountservice.enums.AccountType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+    @RestResource(path="/bytype")
+    List<BankAccount> findByType(@Param("T") AccountType type);
+}
