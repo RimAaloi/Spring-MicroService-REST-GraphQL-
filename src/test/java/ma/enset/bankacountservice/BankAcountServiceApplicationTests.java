0ca9bddbc0a2dc8a25fcb3a0065e2e735b06c223
RimAaloi <aaloirim@example.com>
2025-10-12 21:21:40 +0200

    first commit

---
+++ src/test/java/ma/enset/bankacountservice/BankAcountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.bankacountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAcountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
